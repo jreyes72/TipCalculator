59bd2393bf799821f4ec0824b71ee7ca0a83f0db
Joel Reyes <jreyes@centralstatesser.org>
2021-11-05 13:59:13 -0500

    initial commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Central States SER on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
